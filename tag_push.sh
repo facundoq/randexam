670f5430f845c5ac605e9fcf9c8d9e6fdb568d68
facundoq <facundoq@gmail.com>
2026-03-09 15:18:54 -0300

    tag push

--- tag_push.sh
+++ tag_push.sh
@@ -2,6 +2,12 @@
 VERSION="v$(grep -m 1 '^version =' pyproject.toml | cut -d '"' -f 2)"
 
 echo "Commiting $VERSION..."
+
+if git rev-parse "$VERSION" >/dev/null 2>&1; then
+    echo "Error: Tag $VERSION already exists."
+    exit 1
+fi
+
 echo "Adding all files to git commit..."
 git add .
 git status
